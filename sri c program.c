ec6f4a657bb737513219c908625133e00dce25b8
srinathi2006 <152250641+srinathi2006@users.noreply.github.com>
2025-02-07 04:42:27 -0800

    Add files via upload

---
+++ sri c program.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char a='a';
+    if((a>=65)&&(a<=90))
+    {
+        printf("a is a uppercase alphabet");
+    }
+    else 
+{
+    printf("a is not an uppercase alphabet");
+}
+    return 0;
+}
