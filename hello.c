14190a22cd20964f5f27160ea88a0912976eb0b1
srinathi2006 <152250641+srinathi2006@users.noreply.github.com>
2025-02-07 04:41:10 -0800

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if (num % 3 == 0) {
+        printf("%d is divisible by 3.\n", num);
+    } else {
+        printf("%d is not divisible by 3.\n", num);
+    }
+
+    return 0;
+}
+
+
+ 
