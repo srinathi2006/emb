a8664f05de706407432d0d59f848be293b195c38
srinathi2006 <152250641+srinathi2006@users.noreply.github.com>
2025-02-07 04:42:03 -0800

    Add files via upload

---
+++ myprog.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int roll_no;
+    printf("reverse");
+    for(roll_no=10;roll_no>0;roll_no)
+    {
+        printf("roll_no is %d/n",roll_no);
+    }
+    return 0;
+}
+  
